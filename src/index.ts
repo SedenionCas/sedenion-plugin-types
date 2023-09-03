d94f48fd08a2b752324f6f537e9052c43e58b464
antoKeinanen <anto.keinanen@gmail.com>
2023-09-03 13:17:08 +0300

    feat: export react pluggable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { IPanelPlugin } from "./types/PanelPlugin";
 export { Setting, SettingKind, SettingSection } from "./types/Setting";
 export * from "@tabler/icons-react";
+export * from "react-pluggable";

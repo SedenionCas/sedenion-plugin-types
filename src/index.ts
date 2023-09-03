57196f2426b96069fc17b489fb44118b6fd7645c
antoKeinanen <anto.keinanen@gmail.com>
2023-09-03 13:18:52 +0300

    feat: expose lazy load

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { IPanelPlugin } from "./types/PanelPlugin";
 export { Setting, SettingKind, SettingSection } from "./types/Setting";
 export * from "@tabler/icons-react";
 export * from "react-pluggable";
+export { LazySuspense } from "./LazyLoad";
